733107a58562d32e364b7bd23924d79b26e6dd04
Felipe Pereira <lf.pereirarj@gmail.com>
2019-01-16 16:13:29 -0200

    No geral, acredito que o que ficaria fixo seria o titulo "Meu Carrinho"

--- src/js/components/minicart.js
+++ src/js/components/minicart.js
@@ -14,7 +14,7 @@ class Minicart {
 				<div class="minicart-product__wrapper-flex">
 					<div class="minicart-product__info">
 						<h4 class="minicart-product__name">${item.name}</h4>
-						<strong class="minicart-product__price">R$ ${(item.price / 100).formatMoney()}</strong>
+						<strong class="minicart-product__price">R$${(item.price / 100).formatMoney()}</strong>
 					</div>
 					<button class="minicart-product__remove" type="button" onclick="Minicart.removeItem.apply(null, [${i}])" title="Remover ${item.name} do carrinho"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="10" height="10" viewBox="0 0 10 10"><defs><path id="dmqka" d="M1685 451.047c0 .21-.084.421-.236.573l-1.144 1.144a.816.816 0 0 1-1.145 0L1680 450.29l-2.475 2.474a.816.816 0 0 1-1.145 0l-1.144-1.144a.816.816 0 0 1 0-1.145l2.474-2.475-2.474-2.475a.816.816 0 0 1 0-1.145l1.144-1.144a.816.816 0 0 1 1.145 0l2.475 2.474 2.475-2.474a.816.816 0 0 1 1.145 0l1.144 1.144a.816.816 0 0 1 0 1.145L1682.29 448l2.474 2.475a.816.816 0 0 1 .236.572z"/></defs><g><g transform="translate(-1675 -443)"><use fill="#e75300" xlink:href="#dmqka"/></g></g></svg> Remover</button>
 				</div>
@@ -40,32 +40,36 @@ class Minicart {
 				</span>
             </button>
             <div class="minicart__overlay"></div>
-            <div class="minicart__container">
-                <div class="minicart__header">
-                    <button class="minicart__handle" title="sacola">
-                        <i class="minicart__icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="10" height="16" viewBox="0 0 10 16"><defs><path id="ic9la" d="M1557.914 33.18l1.536 1.544a.59.59 0 0 1 0 .838l-4.942 4.943 4.942 4.942a.59.59 0 0 1 0 .838l-1.536 1.545a.601.601 0 0 1-.847 0l-6.897-6.907a.59.59 0 0 1 0-.837l6.897-6.907a.601.601 0 0 1 .847 0z"/></defs><g><g transform="translate(-1550 -33)"><use fill="#e75300" xlink:href="#ic9la"/></g></g></svg></i>
-                        <span class="minicart__title">Continuar Comprando</span>
-					</button>
-				</div>
-				<div class="minicart__review">
-					<div class="minicart__review-header">
-						<span class="minicart__item-counter">MEU CARRINHO</span>
-						<span class="minicart__item-qty"> (${this.printQuantity(qty)} ITENS)</span>
+			<div class="minicart__container">
+				<div class="minicart__header--fixed">
+					<div class="minicart__header">
+						<button class="minicart__handle" title="sacola">
+							<i class="minicart__icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="10" height="16" viewBox="0 0 10 16"><defs><path id="ic9la" d="M1557.914 33.18l1.536 1.544a.59.59 0 0 1 0 .838l-4.942 4.943 4.942 4.942a.59.59 0 0 1 0 .838l-1.536 1.545a.601.601 0 0 1-.847 0l-6.897-6.907a.59.59 0 0 1 0-.837l6.897-6.907a.601.601 0 0 1 .847 0z"/></defs><g><g transform="translate(-1550 -33)"><use fill="#e75300" xlink:href="#ic9la"/></g></g></svg></i>
+							<span class="minicart__title">Continuar Comprando</span>
+						</button>
 					</div>
-					<div class="minicart__dsp-total">
-						<span class="minicart__dsp-txt">Subtotal do Carrinho:</span>
-						<span class="minicart__dsp-number">${this.getTotal()}</span>
+					<div class="minicart__review">
+						<div class="minicart__review-header">
+							<span class="minicart__item-counter">MEU CARRINHO</span>
+							<span class="minicart__item-qty"> (${this.printQuantity(qty)} ITENS)</span>
+						</div>
+						<div class="minicart__dsp-total">
+							<span class="minicart__dsp-txt">Subtotal do Carrinho:</span>
+							<span class="minicart__dsp-number">${this.getTotal()}</span>
+						</div>
 					</div>
 				</div>
-				<a class="minicart__checkout" href="/Site/Carrinho.aspx">IR PARA O CARRINHO</a>
-                <div class="minicart__content">
-                    <ul class="minicart__products">
-                        ${this.renderItems()}
-                    </ul>
-                </div>
-                <div class="minicart__footer">
-                    <a class="minicart__checkout" href="/Site/Carrinho.aspx">IR PARA O CARRINHO</a>
-                </div>
+				<div class="minicart__content--scroll">
+					<a class="minicart__checkout" href="/Site/Carrinho.aspx">IR PARA O CARRINHO</a>
+					<div class="minicart__content">
+						<ul class="minicart__products">
+							${this.renderItems()}
+						</ul>
+					</div>
+					<div class="minicart__footer">
+						<a class="minicart__checkout" href="/Site/Carrinho.aspx">IR PARA O CARRINHO</a>
+					</div>
+				</div>
             </div>
          </div>
        `;
@@ -99,7 +103,7 @@ class Minicart {
 	getTotal() {
 		const itemsTotal = this.orderForm.totalizers.find(item => item.id === 'Items');
 		const total = itemsTotal ? itemsTotal.value / 100 : 0;
-		return `R$ ${total.formatMoney()}`;
+		return `R$${total.formatMoney()}`;
 	}
 
 	getQuantity() {
@@ -126,5 +130,6 @@ $(document).ready(function() {
 
 	$('body').on('click', '.minicart__handle', function() {
 		$('.minicart').toggleClass('active');
+		$('body').toggleClass('is-fixed');
 	});
 });
