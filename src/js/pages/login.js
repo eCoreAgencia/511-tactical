19a4d15d56090d0b7ad5594bb82e81cccaf4503e
Nicola Regattieri <nicregattieri@gmail.com>
2019-01-18 16:51:10 -0200

    minha conta

--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -1,5 +1,17 @@
-// $(document).ready(function() {
-// 	if ($("body").hasClass("login")) {
-// 		$("").onClick("");
-// 	}
-// });
+$(document).ready(() => {
+	vtexjs.checkout.getOrderForm().done(orderForm => {
+		const ifLogged = orderForm.loggedIn;
+		if (ifLogged) {
+			// desk
+			const emailLimited = orderForm.clientProfileData.email.length;
+			const elementWelcomeMobile = orderForm.clientProfileData.email;
+			if (emailLimited > 15) {
+				console.log(elementWelcomeMobile);
+
+				$(".logged-status").text(
+					`Olá,${elementWelcomeMobile.substr(0, 14)}...`
+				);
+			}
+		}
+	});
+});
