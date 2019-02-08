f868e568fda07790977f3dd65c3497f94def7b5a
Felipe Pereira <lf.pereirarj@gmail.com>
2019-02-08 00:08:53 -0200

    ajuste

--- src/js/components/product.js
+++ src/js/components/product.js
@@ -441,7 +441,6 @@ class Product {
 		$('.zoomPup, .zoomWindow, .zoomPreload').remove();
 
 		$('#image img').unwrap();
-		$('#image img').unwrap();
 
 		$('.thumbs li').each(function () {
 			const img = $('img', this).attr('src');
