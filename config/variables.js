ff41a6a6f49509d1ea112b95dad3ce5b37ae4117
Felipe Pereira <lf.pereirarj@gmail.com>
2019-03-20 19:02:44 -0300

    ajustes

--- config/variables.js
+++ config/variables.js
@@ -2,7 +2,7 @@ const pjson = require('../package.json');
 
 module.exports = {
   PROJECT_ID: "tactical-b2b",
-  TEMPLATE_PREFIX: "eCore-b2b",
+  TEMPLATE_PREFIX: "eCore-b2b-",
   PROJECT_VERSION: pjson.version,
   STORE_ID: "tactical",
   STORE_NAME: "Tactical"
