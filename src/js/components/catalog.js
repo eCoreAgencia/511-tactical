6b4bad37d1c021eb41f43e2e3fd09a33f32ec9d9
Unknown <unknown@example.com>
2019-04-02 19:46:42 -0300

    init

--- src/js/components/catalog.js
+++ src/js/components/catalog.js
@@ -10,5 +10,12 @@ class Catalog {
 }
 
 $(document).ready(() => {
+	if($('body').hasClass('catalog')){
+		$('.product--shelf').each(function(){
+			const name = $('.product__name .product__link', this).text();
 
+			console.log(name);
+			
+		})
+	}
 });
