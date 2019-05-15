7582bd557e7643e6d9680f33f033491fc3ee6e79
Luiz Felipe <lf.pereirarj@gmail.com>
2019-05-15 18:28:26 -0300

    ajustes

--- src/js/components/filter.js
+++ src/js/components/filter.js
@@ -1,47 +1,52 @@
 import {
 	getProductWithVariations,
 	getSearchProducts
-} from '../modules/vtexRequest'
-
+} from "../modules/vtexRequest";
 
 class Filter {
 	constructor() {
-		this.menu = document.querySelector('.logo ');
-		$('.helperComplement').remove();
+		this.menu = document.querySelector(".logo ");
+		$(".helperComplement").remove();
 		this.init();
 		this.clearFilter();
 		this.openFilter();
 		this.clouseFilter();
-		this.currentSearchUrl = '';
-		this.currentPage = '1';
+		this.currentSearchUrl = "";
+		this.currentPage = "1";
 	}
 
 	openFilter() {
-		$('.btnOpenFilter').on('click', function (e) {
+		$(".btnOpenFilter").on("click", function(e) {
 			e.preventDefault();
-			if ($('.category__filter.filter').hasClass('active')) {
-				$(this).html('<i class="icon-filter"></i><p>Mostrar</p><p>Filtros</p>');
-				$('.category__filter.filter').fadeOut();
-				$('.category__filter.filter').removeClass('active');
+			if ($(".category__filter.filter").hasClass("active")) {
+				$(this).html(
+					'<i class="icon-filter"></i><p>Mostrar</p><p>Filtros</p>'
+				);
+				$(".category__filter.filter").fadeOut();
+				$(".category__filter.filter").removeClass("active");
 			} else {
-				$(this).html('<i class="icon-arrow-left"></i><p>Fechar</p><p>Filtros</p>');
-				$('.category__filter.filter').fadeIn();
-				$('.category__filter.filter').addClass('active');
+				$(this).html(
+					'<i class="icon-arrow-left"></i><p>Fechar</p><p>Filtros</p>'
+				);
+				$(".category__filter.filter").fadeIn();
+				$(".category__filter.filter").addClass("active");
 			}
 		});
 	}
 
 	clouseFilter() {
-		$('.clouseFilter').on('click', function (e) {
+		$(".clouseFilter").on("click", function(e) {
 			e.preventDefault();
-			$('.btnOpenFilter').html('<i class="icon-filter"></i><p>Mostrar</p><p>Filtros</p>');
-			$('.category__filter.filter').fadeOut();
-			$('.category__filter.filter').removeClass('active');
+			$(".btnOpenFilter").html(
+				'<i class="icon-filter"></i><p>Mostrar</p><p>Filtros</p>'
+			);
+			$(".category__filter.filter").fadeOut();
+			$(".category__filter.filter").removeClass("active");
 		});
 	}
 
 	clearFilter() {
-		$('.btnClear').on('click', function (e) {
+		$(".btnClear").on("click", function(e) {
 			e.preventDefault();
 			window.location = window.location.href;
 		});
@@ -53,54 +58,54 @@ class Filter {
 		// 	window.location.href = window.location.href + '?PS=12&O=' + value;
 		// });
 
-		const self = this
-
+		const self = this;
 
 		if (this.isExist(this.menu)) {
 			console.log(this.menu);
 		} else {
-			console.log('Não existe');
+			console.log("Não existe");
 		}
 
-		$('.filter .search-multiple-navigator fieldset').each(function () {
-			if ($('div', this).find('label')[0]) {
-				const text = $('h5', this).text();
-				let label = $('div', this).html();
+		$(".filter .search-multiple-navigator fieldset").each(function() {
+			if ($("div", this).find("label")[0]) {
+				const text = $("h5", this).text();
+				let label = $("div", this).html();
 				const html = `
 			  		<li class="filter__item"><span>${text}</span>
 						<div class="filter__options">
 				  			${label}
 						</div>
 			  		</li>`;
-				$('.filter__menu').append(html);
+				$(".filter__menu").append(html);
 			}
 		});
 
-		if($('body').hasClass('category') || $('body').hasClass('department')){
-			$(".filter input[type='checkbox']").vtexSmartResearch();
+		if (
+			$("body").hasClass("category") ||
+			$("body").hasClass("department")
+		) {
+			//$(".filter input[type='checkbox']").vtexSmartResearch();
 		}
 
-
-
-		$('.search-multiple-navigator input').on('change', function () {
-			let urlFilters = ''
-			$('.search-multiple-navigator input').each(function () {
-				if ($(this).is(':checked')) {
-					urlFilters += '&' + $(this).attr('rel');
+		$(".search-multiple-navigator input").on("change", function() {
+			let urlFilters = "";
+			$(".search-multiple-navigator input").each(function() {
+				if ($(this).is(":checked")) {
+					urlFilters += "&" + $(this).attr("rel");
 				}
-			})
-		})
+			});
+		});
 
-		$('.pages li:not(.pgCurrent)').on('click', function(){
+		$(".pages li:not(.pgCurrent)").on("click", function() {
 			const value = $(this).text();
-			if(value === 'anterior'){
-				self.loadMoreProducts(-1)
-			} else if (value === 'próximo') {
-				self.loadMoreProducts(+1)
+			if (value === "anterior") {
+				self.loadMoreProducts(-1);
+			} else if (value === "próximo") {
+				self.loadMoreProducts(+1);
 			} else {
-				self.loadMoreProducts(value)
+				self.loadMoreProducts(value);
 			}
-		})
+		});
 	}
 
 	isExist(e) {
@@ -111,44 +116,44 @@ class Filter {
 	getUrl(scroll) {
 		var s = scroll || false;
 		if (s)
-			return this.currentSearchUrl.replace(/PageNumber=[0-9]*/, 'PageNumber=' + this.currentPage);
-		else
-			return (searchUrl).replace(
+			return this.currentSearchUrl.replace(
 				/PageNumber=[0-9]*/,
-				'PageNumber=' + pageNumber
+				"PageNumber=" + this.currentPage
+			);
+		else
+			return searchUrl.replace(
+				/PageNumber=[0-9]*/,
+				"PageNumber=" + pageNumber
 			);
 	}
 
 	getSearchUrl() {
 		var url, content, preg;
-		jQuery('script:not([src])').each(function() {
+		jQuery("script:not([src])").each(function() {
 			content = jQuery(this)[0].innerHTML;
 			preg = /\/buscapagina\?.+&PageNumber=/i;
 			if (content.search(/\/buscapagina\?/i) > -1) {
 				url = preg.exec(content);
-
 			}
 		});
-		if (typeof url !== 'undefined' && typeof url[0] !== 'undefined') return url[0];
+		if (typeof url !== "undefined" && typeof url[0] !== "undefined")
+			return url[0];
 		else {
 			log(
-				'Não foi possível localizar a url de busca da página.\n Tente adicionar o .js ao final da página. \n[Método: getSearchUrl]'
+				"Não foi possível localizar a url de busca da página.\n Tente adicionar o .js ao final da página. \n[Método: getSearchUrl]"
 			);
-			return '';
+			return "";
 		}
 	}
 
-	loadMoreProducts(page){
+	loadMoreProducts(page) {
 		const url = this.getSearchUrl();
 		console.log(url);
 	}
 }
 
-$(document).ready(function () {
-	if ($('body').hasClass('catalog')) {
+$(document).ready(function() {
+	if ($("body").hasClass("catalog")) {
 		window.filter = new Filter();
-
-
 	}
 });
-
